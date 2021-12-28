b64716b4035c6e32f2e5d959f65593f7c8b8bae5
ChocoAlmendrado <pss1005@alu.ubu.es>
2021-12-28 01:01:01 +0100

    Basic UI added

---
+++ Assets/Classes/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class User
+    {
+        private int id;
+        private String name;
+    }
+}
